29cbab0b72e392d1936c19ed625028ebc04e5a40
elin281 <herlinda.e22@gmail.com>
2023-03-15 21:32:24 +0700

    Upload file tugas 4 js

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,78 @@
+//Fungsi Pertambahan
 function addition(){
     var frm = document.getElementById('calculator');
     var n1 = parseFloat(frm.number1.value);
     var n2 = parseFloat(frm.number2.value);
-    var total = n1 + n2;
-    frm.result.value = total;
+    var result = n1 + n2;
+    frm.result.value = result;
+    if (isNaN(result)){
+        document.getElementById("calculator").result.value = "";
+        alert("Please, complete your input data!");
+    }
+    else{
+        document.getElementById("calculator").result.value = result;
+    }
 }
+//Fungsi Pengurangan
 function subtraction(){
     var frm = document.getElementById('calculator');
     var n1 = parseFloat(frm.number1.value);
     var n2 = parseFloat(frm.number2.value);
-    var total = n1 - n2;
-    frm.result.value = total;
+    var result = n1 - n2;
+    frm.result.value = result;
+    if (isNaN(result)){
+        document.getElementById("calculator").result.value = "";
+        alert("Please, complete your input data!");
+    }
+    else{
+        document.getElementById("calculator").result.value = result;
+    }
+}
+//Fungsi Pembagian
+function division(){
+    var frm = document.getElementById('calculator');
+    var n1 = parseFloat(frm.number1.value);
+    var n2 = parseFloat(frm.number2.value);
+    var result = n1 / n2;
+    frm.result.value = result;
+    if (isNaN(result)){
+        document.getElementById("calculator").result.value = "";
+        alert("Please, complete your input data!");
+    }
+    else{
+        document.getElementById("calculator").result.value = result;
+    }
+}
+//Fungsi Perkalian
+function multiplication(){
+    var frm = document.getElementById('calculator');
+    var n1 = parseFloat(frm.number1.value);
+    var n2 = parseFloat(frm.number2.value);
+    var result = n1 * n2;
+    frm.result.value = result;
+    if (isNaN(result)){
+        document.getElementById("calculator").result.value = "";
+        alert("Please, complete your input data!");
+    }
+    else{
+        document.getElementById("calculator").result.value = result;
+    }
+}
+//Fungsi Perpangkatan
+function exponent(){
+    var frm = document.getElementById('calculator');
+    var n1 = parseFloat(frm.number1.value);
+    var n2 = parseFloat(frm.number2.value);
+    //Menggunakan simbol operasi aritmatika dasar
+    //var result = n1 ** n2;
+    //Menggunakan fungsi Math.pow()
+    var result = Math.pow(n1,n2);
+    frm.result.value = result;
+    if (isNaN(result)){
+        document.getElementById("calculator").result.value = "";
+        alert("Please, complete your input data!");
+    }
+    else{
+        document.getElementById("calculator").result.value = result;
+    }
 }
