06bad947b6e8370fad7bbfd1a0e87dfe037d365a
elin281 <herlinda.e22@gmail.com>
2023-04-04 15:14:40 +0700

    Upload file tugas pertemuan 9

---
+++ webnative/admin/index.php
@@ -0,0 +1,26 @@
+<?php
+    include_once("top.php");
+    include_once("menu.php");
+ ?>
+ <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">  
+                        <?php
+                            error_reporting(0);
+                            $url = $_GET['url'];
+                            if($url == 'dashboard'){
+                                include_once("dashboard.php");
+                            }
+                            else if(!empty($url)){
+                                include_once("".$url.".php");
+                            }
+                            else{
+                                "dashboard.php";
+                            }
+                         ?>
+                    </div>
+                </main>
+</div>
+<?php
+    include_once("bottom.php");
+ ?>
